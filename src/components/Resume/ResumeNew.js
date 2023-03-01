afd022c1f77ebef35807087151dcd3fd7d121d6c
ritik sharma <bharadwajritik1@gmail.com>
2023-03-02 03:44:56 +0530

    "updated resume"

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Ritik_Sharma.pdf";
+import pdf from "../../Assets/../Assets/ritiksharma.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
